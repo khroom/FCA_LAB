975322b34fcfb9147886266644796132eef9a3bc
Anna <dazzzkl@yandex.ru>
2021-10-11 22:04:54 +0700

    Поправлена функция корректировки первоначального датафрейма

--- arl_data.py
+++ arl_data.py
@@ -5,18 +5,15 @@ import unidecode
 class Data:
     
     @staticmethod
-    def fix_initial_frame(df: pd.DataFrame, fullness=0.9):
-        service_cols = ['Индекс', 'АЭ: кол-во', 'АЭ: длит.', 'АЭ: напр. ср.', 'Пена: снято с эл-ра', 'Срок службы',
-                        'Кол-во поддер. анодов', 'АЭ: кол-во локальных', 'Отставание (шт)', 'Другое нарушение (шт)',
-                        'Нарушение', 'Анод', 'Выливка: отношение скорости', 'Выход по току: Л/О', 'Подина: напр.',
-                        'Кол-во доз АПГ в руч.реж.', 'Конус (да/нет)']
-        result_frame = df.drop(service_cols, axis='columns')
-        not_full_columns = []
-        for column in result_frame.columns:
-            if result_frame[column].count() / len(result_frame) < fullness:
-                not_full_columns.append(column)
-        result_frame = result_frame.drop(not_full_columns, axis='columns')
-        for column in result_frame.columns:
-            result_frame.rename(columns={column: unidecode.unidecode(column).replace('\'', '')}, inplace=True)
-        result_frame = result_frame.set_index(['Nomer elektrolizera', 'Data'])
-        return result_frame
+    def fix_initial_frame(df: pd.DataFrame, param_names: list, faults_names: list, cell_name: str, date_name: str, fullness=0.9):
+        counts = df[param_names].count()
+        columns = counts[counts >= len(df) * fullness].index.to_list()
+        columns = columns + faults_names
+        columns.append(cell_name)
+        columns.append(date_name)
+        result = df.reset_index()[columns]
+        for column in result.columns:
+            result.rename(columns={column: unidecode.unidecode(column).replace('\'', '')}, inplace=True)
+        uni_cell_name = unidecode.unidecode(cell_name).replace('\'', '')
+        uni_date_name = unidecode.unidecode(date_name).replace('\'', '')
+        return result.set_index([uni_cell_name, uni_date_name])
